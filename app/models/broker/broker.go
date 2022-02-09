c4ba433497ad996a396cee085e81a4196a24a667
Chumak Nadiia <chumaknadya19@gmail.com>
2022-02-09 16:02:13 +0200

    feat conclude channel

--- app/models/broker/broker.go
+++ app/models/broker/broker.go
@@ -1,16 +1,40 @@
 package broker
 
 import (
-	"github.com/statechannels/go-nitro/types"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	typ "github.com/statechannels/go-nitro/types"
 )
 
+var ChainId = big.NewInt(1337)
+
 type Broker struct {
-	Address     types.Address
-	Destination types.Destination
+	Address     typ.Address
+	Destination typ.Destination
 	PrivateKey  []byte
 	Role        uint
 }
 
-func New(address types.Address, destination types.Destination, privateKey []byte, role uint) *Broker {
+func New(address typ.Address, destination typ.Destination, privateKey []byte, role uint) *Broker {
 	return &Broker{Address: address, Destination: destination, PrivateKey: privateKey, Role: role}
+}
+
+func (b *Broker) SignTransaction(address common.Address, tx *types.Transaction) (*types.Transaction, error) {
+	signer := types.LatestSignerForChainID(ChainId)
+	hash := signer.Hash(tx)
+
+	prv, err := crypto.ToECDSA(b.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	signature, err := crypto.Sign(hash.Bytes(), prv)
+	if err != nil {
+		return nil, err
+	}
+
+	return tx.WithSignature(signer, signature)
 }
